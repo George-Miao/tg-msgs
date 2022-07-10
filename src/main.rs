9502d2b42599abf8b11c095ae4629a6d9713f761
George Miao <gm@miao.dev>
2022-07-11 01:43:26 +0700

    feat: use dotenv

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ use telegram_chat::Analyzer;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    dotenv::dotenv()?;
 
     let content = std::fs::read("data/result.json")?;
     let data = serde_json::from_slice(&content)?;
     let file = File::create("data/output.md")?;
+    let opt_out = std::env::var("OPT_OUT")
+        .map(|x| {
+            x.split(',')
+                .map(|x| {
+                    x.trim()
+                        .parse::<i64>()
+                        .expect("invalid opt_out: requires `i64`")
+                })
+                .collect::<Vec<_>>()
+        })
+        .unwrap_or_default();
 
     Analyzer::new(&data)
         .wrap_with("```")
         .take(15)
         .write_to(file)
-        .opt_out(&[223683261])
+        .opt_out(&opt_out)
         .count_link()?
         .sender_rank()?
         .count_substring("草")?;
